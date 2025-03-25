fadfaec4396c56a2f18d708c08947b5c7f932936
Vincenzo Ciaccio <vincenzo.ciaccio@yotoplay.com>
2025-03-25 20:03:35 +0000

    first logic scaffold

--- shared/rooms/echo/config.ts
+++ shared/rooms/echo/config.ts
@@ -1,3 +1,5 @@
+import type { Client } from "../../Client";
+import type { ActionPayload } from "../../ws";
 import type { RoomConfig } from "../configs";
 
 export type EchoRoomState = {
@@ -11,3 +13,20 @@ export interface EchoRoomConfig extends RoomConfig<EchoRoomState> {
 
 export const echoRoomActions = ["shout"] as const;
 export type EchoRoomActions = (typeof echoRoomActions)[number];
+
+export type ShoutPayload = {
+  msg: string;
+};
+
+export function actionsHandler(
+  client: Client,
+  currentState: EchoRoomState,
+  action: ActionPayload<ShoutPayload>
+): EchoRoomState {
+  if (action.data?.msg) {
+    const msg = `${client.socketId}: ${action.data.msg}`;
+    const echo = `room: ${action.data.msg}`;
+    currentState.messages = [...currentState.messages, msg, echo];
+  }
+  return currentState;
+}
