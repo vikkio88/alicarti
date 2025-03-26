b6c6a1b6ca456e8ebc68f9fcdc57467cba61eb79
Vincenzo Ciaccio <vincenzo.ciaccio@yotoplay.com>
2025-03-26 19:40:25 +0000

    moving websocket logic handler back to index as I need server in context

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,7 +1,9 @@
-import type { Client } from "@alicarti/shared";
+import { parseMessage, setup, type Client } from "@alicarti/shared";
 import { staticServe } from "./servers/static";
-import { websocketServe, type WsServerConfig } from "./servers/websocket";
 import { websocketUpgrade } from "./servers/upgrade";
+import { Topic, TopicManager } from "./libs/Topic";
+import { messageHandler, type ServerContext } from "./libs/messageHandler";
+import { ClientsManager } from "./libs/ClientsManager";
 
 const CORS_HEADERS = {
   headers: {
@@ -11,14 +13,52 @@ const CORS_HEADERS = {
   },
 };
 
+export type WsServerConfig = {
+  log: (...strings: string[]) => void;
+};
+
 const wsConfig: WsServerConfig = {
   log: (message: string) => console.log(`[server] ${Date.now()} - ${message}`),
 };
 
+const clientsManager = new ClientsManager();
+const topicsManager = new TopicManager([new Topic("broadcast", false)]);
+const broadcastTopic = topicsManager.byName("broadcast")!;
+const ctx: ServerContext = {
+  clients: clientsManager,
+  topics: topicsManager,
+  logger: wsConfig.log,
+};
+
 const server = Bun.serve<Client>({
   static: staticServe,
-  websocket: websocketServe(wsConfig),
+  websocket: {
+    async message(ws, msg) {
+      ctx.server = server;
 
+      const message = parseMessage(msg as string);
+      ctx.logger(`received message "${message.type}" from ${ws.data.socketId}`);
+      messageHandler(ws, message, ctx);
+    },
+    async open(ws) {
+      ctx.server = server;
+
+      ctx.logger(`client connected: ${ws.data.socketId}`);
+      ctx.clients.onConnect(ws);
+      ctx.clients.joinTopic(broadcastTopic, ws);
+      ws.send(setup({ ...ws.data }));
+    },
+    async close(ws) {
+      ctx.server = server;
+      
+      ctx.logger(`client disconnected: ${ws.data.socketId}`);
+      const topics = clientsManager.getClientTopics(ws);
+      ctx.topics.getManyByName(topics).forEach((t) => {
+        ctx.clients.leaveTopic(t, ws);
+      });
+      ctx.clients.onDisconnect(ws);
+    },
+  },
   async fetch(req, server) {
     // Handle CORS preflight requests
     if (req.method === "OPTIONS") {
