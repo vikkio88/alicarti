60392ac1b346ee699f246f340845f2d82f4c932f
Vincenzo Ciaccio <vincenzo.ciaccio@yotoplay.com>
2025-03-28 19:32:23 +0000

    changing id generator

--- be/src/libs/idGenerators.ts
+++ be/src/libs/idGenerators.ts
@@ -1,13 +1,13 @@
-import { ulid } from "ulid";
-
-const id = () => ulid().toString();
+import { nanoid } from "nanoid";
+const ID_LENGTH = 5;
+const id = () => nanoid(ID_LENGTH).toString();
 
 const CLIENT_ID_PREFIX = "c_";
 export function clientId() {
   return `${CLIENT_ID_PREFIX}${id()}`;
 }
 
-const TOPIC_ID_PREFIX = "r_";
+const TOPIC_ID_PREFIX = "t_";
 export function topicId() {
   return `${TOPIC_ID_PREFIX}${id()}`;
 }
