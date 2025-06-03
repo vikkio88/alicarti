20869c921e44f095733224f13034687376603836
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2025-06-03 20:22:08 +0100

    fixed lobby style

--- web/src/components/Lobby.svelte
+++ web/src/components/Lobby.svelte
@@ -9,95 +9,82 @@
   let canCreateRoom = check(connection.info().connection);
 
   let showJoin = $state(false);
+  let showCreate = $state(false);
 </script>
 
-<main class="responsive">
-  <article class="medium middle-align center-align">
-    <div>
-      <div class="row join" class:showJoin={!showJoin}>
-        {#if showJoin}
-          <form
-            onsubmit={(e) => {
-              e.preventDefault();
-              if (roomId.length < 3) return;
-              connection.command(Commands.JOIN_ROOM, { roomId });
-            }}
-          >
-            <div class="field">
-              <input
-                name="roomId"
-                type="text"
-                bind:value={roomId}
-                placeholder="Room Id"
-              />
-            </div>
-            <button class="f1" disabled={roomId.length < 3} type="submit">
-              <div class="tooltip">Join</div>
-              <Icon name="enter" />
-            </button>
-          </form>
-        {/if}
-        <button onclick={() => (showJoin = !showJoin)}>
-          {#if showJoin}
+<main class="responsive fcc">
+  <div class="f c g">
+    {#if !showJoin && !showCreate}
+      <button class="small-round" onclick={() => (showJoin = true)}>
+        <Icon name="enter" /> Join Room
+      </button>
+
+      <button class="small-round" onclick={() => (showCreate = true)}>
+        <Icon name="create" /> Create Room
+      </button>
+    {/if}
+
+    {#if showJoin}
+      <form
+        class="fcc"
+        onsubmit={(e) => {
+          e.preventDefault();
+          if (roomId.length < 3) return;
+          connection.command(Commands.JOIN_ROOM, { roomId });
+        }}
+      >
+        <div class="field border">
+          <input
+            name="roomId"
+            type="text"
+            bind:value={roomId}
+            placeholder="Room Id"
+            autocomplete="off"
+          />
+        </div>
+        <div>
+          <button class="f1" disabled={roomId.length < 3} type="submit">
+            <div class="tooltip">Join</div>
+            <Icon name="enter" />
+          </button>
+          <button class="f1" onclick={() => (showJoin = false)}>
             <Icon name="x" />
             <div class="tooltip">Cancel</div>
-          {:else}
-            <Icon name="enter" /> Room
-            <div class="tooltip right">Join Room</div>
-          {/if}
-        </button>
-      </div>
+          </button>
+        </div>
+      </form>
+    {/if}
 
-      <div class="row create">
-        <fieldset>
-          <div class="row">
-            <div class="field suffix border large">
-              <span class="helper">Room Type</span>
-              <select
-                bind:value={roomType}
-              >
-                <option>{RoomTypes.chat}</option>
-                <option>{RoomTypes.echo}</option>
-                <option>{RoomTypes.rockPaperScissor}</option>
-              </select>
-              <i>arrow_drop_down</i>
-            </div>
+    {#if showCreate}
+      <div class="frc g">
+        <div class="row">
+          <div class="field suffix border large">
+            <span class="helper">Room Type</span>
+            <select bind:value={roomType}>
+              <option>{RoomTypes.chat}</option>
+              <option>{RoomTypes.echo}</option>
+              <option>{RoomTypes.rockPaperScissor}</option>
+            </select>
+            <i>arrow_drop_down</i>
           </div>
-          <div class="row right-align">
-            <button
-              disabled={!canCreateRoom}
-              onclick={() =>
-                connection.command(Commands.CREATE_ROOM, {
-                  roomType,
-                })}
-            >
-              <Icon name="create" /> Room
-            </button>
-          </div>
-        </fieldset>
+        </div>
+        <div class="row right-align">
+          <button
+            disabled={!canCreateRoom}
+            onclick={() =>
+              connection.command(Commands.CREATE_ROOM, {
+                roomType,
+              })}
+          >
+            <Icon name="create" />
+            <div class="tooltip">Create</div>
+          </button>
+          <button class="f1" onclick={() => (showCreate = false)}>
+            <Icon name="x" />
+            <div class="tooltip">Cancel</div>
+          </button>
+        </div>
       </div>
-    </div>
-  </article>
+    {/if}
+  </div>
 </main>
-
-<style>
-  form {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    gap: 0.5rem;
-  }
-
-  .join {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .showJoin {
-    display: block;
-    padding-left: 0.5rem;
-  }
-</style>
